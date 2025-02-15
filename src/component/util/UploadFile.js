1c45397cb479579406c0b6f814fc22eef4773db1
mikhaildh20 <mikhaildaffa7@gmail.com>
2025-02-15 16:14:11 +0700

    update

--- src/component/util/UploadFile.js
+++ src/component/util/UploadFile.js
@@ -4,16 +4,16 @@ import { API_LINK } from "./Constants";
 const uploadFile = async (file) => {
   if (file != null) {
     const data = new FormData();
-    const jwtToken = Cookies.get("jwtToken");
+    // const jwtToken = Cookies.get("jwtToken");
     data.append("file", file.files[0]);
 
     try {
       const response = await fetch(API_LINK + "Upload/UploadFile", {
         method: "POST",
         body: data,
-        headers: {
-          Authorization: "Bearer " + jwtToken,
-        },
+        // headers: {
+        //   Authorization: "Bearer " + jwtToken,
+        // },
       });
       const result = await response.json();
       if (response.ok) {
